dfcf884831c23a0eb3217f59f12e237e83ede6f5
Filipe Cristian <filipeecristian@gmail.com>
2023-12-29 09:04:14 -0300

    removed dist

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app = require("./app");
-const app_ws_1 = require("./app-ws");
-const server = app.default.listen(process.env.PORT || 3000, () => {
-    console.log(`App Express is running!`);
-});
-(0, app_ws_1.websocket)(server);
-//# sourceMappingURL=index.js.map
