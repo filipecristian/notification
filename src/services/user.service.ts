b403a7fe4038b9b09912dc1970858ee461df6f9f
Filipe Cristian <filipeecristian@gmail.com>
2024-01-18 19:54:38 -0300

    Removed comment

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,9 @@
 import { LoginRequest } from "../http/requests/login.request";
 import { User } from '../models/user.model';
-import { Sequelize } from "sequelize";
 
 export class UserService {
 
     async login(req: LoginRequest) : Promise<boolean> {
-        console.log("BBBB", req.company);
         const user = await User.findOne(
             {
                 where: {
@@ -14,9 +12,6 @@ export class UserService {
                 }
             }
         );
-
-        console.log(user, "AAAAA");
-        
         return user !== null;
     }
 }
