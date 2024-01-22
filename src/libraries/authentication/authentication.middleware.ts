14ed13a4c7cf2cf06f846a4a710c1267d725ffc1
Filipe Cristian <filipeecristian@gmail.com>
2024-01-22 20:24:45 -0300

    Removed unset itens

--- src/libraries/authentication/authentication.middleware.ts
+++ src/libraries/authentication/authentication.middleware.ts
@@ -1,14 +1,13 @@
-import { Router, Request, Response } from 'express'
+import { Request, Response } from 'express'
 import { AuthenticationService } from './authentication.service'
 
 export function authenticate(req: Request, res: Response, next: any) {
     try {
-        const authenticationService = new AuthenticationService;
         const service = new AuthenticationService;
         const token  = req.headers.authorization;
-        if (typeof token === 'string') {
+        if (token) {
             const decoded = service.getDataByToken(token);
-            if (typeof decoded === 'object') {
+            if (decoded) {
                 next();
             }
         }
